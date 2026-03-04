5e8f5a0f6880bb75b573d0dc7477a380176e6b25
tp19-beep <tp19@kent.ac.uk>
2026-03-04 18:22:54 +0000

    def main() started

---
+++ (Main)CollectiblesCollection.py
@@ -0,0 +1,20 @@
+def main():
+    while True:
+        print("\n!!Welcome to the Collectors Vault!!")
+        print("A manager for collectibles and toys from the 2000s and 2010s!")
+        print("LOADING...")
+        print("...")
+
+        print("\nPlease select an option: ")
+        print("1. View Collection")
+        print("2. Add Collectible")
+        print("3. Remove Collectible")
+        print("4. Search Collection")
+        print("5. Update Collectible")
+        print("6. Count Collectibles")
+        print("7. Give me a random Collectible")
+        print("8. Exit.")
+
+        choice = input("Choose an option between 1 and 8.")
+        if choice == '1': 
+            
