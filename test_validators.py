d72023050d902ee4f98ec09a57288152e9f91f2d
tp19-beep <tp19@kent.ac.uk>
2026-03-06 20:36:17 +0000

    test_validators.py added for pytest

---
+++ test_validators.py
@@ -0,0 +1,22 @@
+from Validators import valid_id, valid_price, valid_year
+
+def test_valid_id():
+    assert valid_id("12345") == True
+    assert valid_id("abcde") == False
+    assert valid_id("1234") == False
+    assert valid_id("123456") == False
+    assert valid_id("12a45") == False
+
+def test_valid_price():
+    assert valid_price("10.99") == True
+    assert valid_price("10") == True
+    assert valid_price("10.999") == False
+    assert valid_price("abc") == False
+
+def test_valid_year():
+    assert valid_year("2020") == True
+    assert valid_year("20200") == False
+    assert valid_year("202") == False
+    assert valid_year("abc") == False
+
+    
