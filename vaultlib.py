b37f0cb4d5e56f8e2990491a0dbffcaa2fbdadf3
tp19-beep <tp19@kent.ac.uk>
2026-03-10 17:01:30 +0000

    fixed class collectible to include value errors

--- vaultlib.py
+++ vaultlib.py
@@ -2,6 +2,20 @@ import re
 
 class Collectible:
     def __init__(self, brand, name, year, price, rarity, number_id ):
+
+        if not brand:
+            raise ValueError("Brand cannot be empty")
+        if not name: 
+            raise ValueError("Name cannot be empty")
+        if not year: 
+            raise ValueError("Year must be a 4-digit number.")
+        if not price: 
+            raise ValueError("Price must be a valid number with up to 2 decimal places")
+        if not rarity: 
+            raise ValueError("Rarity must be one of: Common, Uncommon, Rare, Very Rare, Ultra Rare.")
+        if not number_id: 
+            raise ValueError("ID must be a 5-digit number.")
+
         self.brand = brand
         self.name = name
         self.year = year 
