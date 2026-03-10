eec8a632b8c73d3c86b667d2b465f77d96bc54f0
tp19-beep <tp19@kent.ac.uk>
2026-03-10 16:20:22 +0000

    OOP class Collectible added to vaultlib

--- vaultlib.py
+++ vaultlib.py
@@ -1,5 +1,15 @@
 import re
 
+class Collectible:
+    def __init__(self, brand, name, year, price, rarity, number_id ):
+        self.brand = brand
+        self.name = name
+        self.year = year 
+        self.price = price
+        self.rarity = rarity 
+        self.number_id = number_id
+            
+
 def valid_id(number_id):
     if re.fullmatch(r"\d{5}", number_id):
         return True
@@ -14,3 +24,4 @@ def valid_year(year):
     if re.fullmatch(r"\d{4}", year):
         return True
     return False
+
