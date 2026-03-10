9b9289b2ac72f97b7437c14e03b940f02385f53f
tp19-beep <tp19@kent.ac.uk>
2026-03-10 17:24:08 +0000

    used property and setter

--- vaultlib.py
+++ vaultlib.py
@@ -29,7 +29,63 @@ class Collectible:
             f"{self.rarity} | ${self.price:.2f} USD | ID: {self.number_id}"
         )
 
+    @property
+    def brand(self): 
+        return self._brand
+
+    @brand.setter
+    def brand(self, value): 
+        if not value: 
+            raise ValueError("Brand Cannot be Empty.")
+        self._brand = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value): 
+        if not value: 
+            raise ValueError("Name cannot be Empty.")
+        self._brand = value
+
+    @property
+    def year(self): 
+        return self._year
+
+    @year.setter
+    def year(self, value): 
+        if not value:
+            raise ValueError("Year must be a 4-digit number.")
+        self._year = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not value:
+            raise ValueError("Price must be a valid number with up to 2 decimal places")
+        self._price = value
+
+    @property 
+    def rarity(self): 
+        return self._rarity
+
+    @rarity.setter
+    def rarity(self, value): 
+        if not value: 
+            raise ValueError("Rarity must be one of: Common, Uncommon, Rare, Very Rare, Ultra Rare.")
         
+    @property
+    def number_id(self): 
+        return self._number_id
+    
+    @number_id.setter
+    def number_id(self, value):
+        if not value:
+            raise ValueError("ID must be a 5-digit number.")
             
 
 def valid_id(number_id):
